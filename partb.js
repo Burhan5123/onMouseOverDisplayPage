ec90d68c278959ae63b99849f25a90e66bff9d04
Burhan5123 <burhanuddin54ezzy@gmail.com>
2023-06-24 23:02:30 -0600

    first commit

---
+++ partb.js
@@ -0,0 +1,61 @@
+function hide_text(id){
+
+	if(id=="img1"){
+	document.getElementById("1").style.visibility = "hidden";
+	}
+
+	else if(id=="img2"){
+	document.getElementById("2").style.visibility = "hidden";
+	}
+
+	else if(id=="img3"){
+	document.getElementById("3").style.visibility = "hidden";
+	}
+
+	else if(id=="img4"){
+	document.getElementById("4").style.visibility = "hidden";
+	}
+
+	else if(id=="img5"){
+	document.getElementById("5").style.visibility = "hidden";
+	}
+
+	else if(id=="img6"){
+	document.getElementById("6").style.visibility = "hidden";
+	}
+	
+}
+
+function unhide_text(id){
+
+	if(id=="img1"){
+	document.getElementById("1").style.visibility = "visible";
+	}
+
+	else if(id=="img2"){
+	document.getElementById("2").style.visibility = "visible";
+
+	}
+
+	else if(id=="img3"){
+	document.getElementById("3").style.visibility = "visible";
+
+	}
+
+	else if(id=="img4"){
+	document.getElementById("4").style.visibility = "visible";
+
+	}
+
+	else if(id=="img5"){
+	document.getElementById("5").style.visibility = "visible";
+
+	}
+
+	else if(id=="img6"){
+	document.getElementById("6").style.visibility = "visible";
+
+	}
+
+
+}
